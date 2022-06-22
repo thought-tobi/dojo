348993eee4086bf00aa9ec280f2af1b9d4f11c1c
Elisa Ebler <elisa.ebler@Elisas-MacBook-Pro.local>
2022-06-22 16:34:28 +0200

    Mob Session - Testing the anagram method

--- anagram/anagram_test.py
+++ anagram/anagram_test.py
@@ -1,9 +1,18 @@
 import unittest
 
+from anagram import is_anagram
+
 
 class MyTestCase(unittest.TestCase):
     def test_something(self):
         self.assertEqual(True, False)  # add assertion here
+
+    def test_anagram(self):
+        self.assertEqual(is_anagram("silent","listen" ), True)
+        self.assertEqual(is_anagram("sile", "listen"), False)
+        self.assertEqual(is_anagram("silenx", "listen"), False)
+        self.assertEqual(is_anagram("", "listen"), False)
+        self.assertEqual(is_anagram("", ""), True)
 
 
 if __name__ == '__main__':
