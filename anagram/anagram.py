8a84310bc956ed16dbf81540290bddb228554d11
elisa-ebler <elisa.ebler@protonmail.com>
2022-06-22 16:38:45 +0200

    Mob Session - Added method for checking if two words are an anagram

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -2,5 +2,15 @@ def main():
     pass
 
 
+def is_anagram(x: str, y:str) -> bool:
+    if len(x) != len(y):
+        return False
+    else:
+     if sorted(x) == sorted(y):
+         return True
+     else:
+         return False
+
+
 if __name__ == '__main__':
     main()
